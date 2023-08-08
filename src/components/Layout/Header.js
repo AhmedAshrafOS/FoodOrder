d8c088e99e47b53df8abd14daf26d47244d16c68
AhmedAshrafOS <nad.boyo@gmail.com>
2023-08-08 22:04:23 +0300

    Finish LayOut

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+
+import mealsImage from '../../assets/meals.jpg'
+import cLasses from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props => {
+    return (
+        <Fragment>
+            <header className={cLasses.header}>
+                <h1>ElSultan Meals</h1>
+                <HeaderCartButton></HeaderCartButton>
+            </header>
+            <div className={cLasses['main-image']}>
+                <img src={mealsImage} alt="Meals" />
+            </div>
+        </Fragment>
+    );
+};
+
+export default Header;
